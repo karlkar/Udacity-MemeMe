05b52d0b28232df8c2c7d1e5a327ec6324486816
Karol Ksionek <karol.ksionek@vimn.com>
2021-07-26 22:48:15 +0200

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ksionek, Karol on 26/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
